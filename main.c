e7eb1b97b803534ab5aa0d8d69765275aa238dc9
thddlrla <okjh0999@naver.com>
2023-10-13 16:36:22 +0900

    creat main c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+  
+void print_star(void)
+{
+	
+   int i; 
+   
+   for(i=0;i<10;i++) 
+         printf("*");
+
+}
+
+int main(int argc, char *argv[]) {
+	
+  
+     
+    int i;
+     
+    print_star();
+    print_star();
+    print_star();
+     
+    system("PAUSE");
+	return 0;
+}
